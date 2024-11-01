b10382874d25d113b1a804938359ac5383d896bf
Fatim-coding <timabadjie12@gmail.com>
2024-11-01 15:15:06 +0000

    second commit

---
+++ main.py
@@ -0,0 +1,22 @@
+a = 16
+b = 19
+c = 0
+
+if a and b and c:
+    print("all numbers value as true")
+else:
+    print("atlest one number value as false")
+
+
+a = 12
+b = -12
+c = 0
+
+if a > 0 or b > 0:
+    print("Either of the numbers is greater than 0")
+else:
+     print("12 is greater than 0")
+if b > 0 or c > 0:
+     print("either of the numbers is greater than 0")
+else:
+     print("12 is greater than 0")
